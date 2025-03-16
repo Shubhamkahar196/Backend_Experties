46f8228727826f4bfa527429f4d3f59f6f58b64a
Shubham kahar <Shubhamkahar196@gmail.com>
2025-03-16 22:11:20 +0530

    configure

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from 'express';
-
+import cors from 'cors';
+import cookieParser from 'cookie-parse';
 
 const app = express();
 
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
+
+app.use(express.json({limit: "16kb"}))  // kinta request lena hai
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export { app }
