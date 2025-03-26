47b911b7b358f196f887e736d75b5fc72410f41c
Shubham kahar <Shubhamkahar196@gmail.com>
2025-03-26 20:19:18 +0530

    checking email,password,fullname

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,11 +4,23 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok",
+    // get user details from frontend (Postman)
+    //validation - not empty 
+    //check if user already exists: username,email
+    //check for images,check for avatar
+    //upload them to cloudinary, avatar
+    //create user object - create entry in db
+    //remove password and refresh token field from response
+    //check for user creation
+    //return res
 
-    })
-})
+    const {fullname,email,password } = req.body;
+    console.log("email:",email);
+
+
+
+
+});
 
 
 export { registerUser }
