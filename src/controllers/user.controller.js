3032b195d68cadda9a8095a429b1a23a8ab35281
Shubham Kahar <132445401+Shubhamkahar196@users.noreply.github.com>
2025-06-10 21:00:53 +0530

    Update user.controller.js
    removing unused code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,49 +2,6 @@
 // this is for error handling and avoid writing try catch every time
 import { asyncHandler } from '../utils/asyncHandler.js'
 
-// import {ApiError} from '../utils/apiError.js'
-// import {User } from "../models/user.model.js"
-
-// const registerUser = asyncHandler(async (req, res) => {
-    // get user details from frontend (Postman)
-    //validation - not empty 
-    //check if user already exists: username,email
-    //check for images,check for avatar
-    //upload them to cloudinary, avatar
-    //create user object - create entry in db
-    //remove password and refresh token field from response
-    //check for user creation
-    //return res
-
-    // const {fullname,email,password } = req.body;
-    // console.log("email:",email);
-    // console.log("fullname:",fullname);
-    // console.log("password:",password);
-     
-    // if(fullname ===""){
-    //     throw new ApiError(400,"fullname is required")
-    // }
-
-    // if(
-    //     [fullname,email,username,password].some((field)=>
-    //         field?.trim()===""
-    //     )
-    // ){
-    //     throw new ApiError(400, "All fields are required")
-    // }
-
-
-
-// });
-
-// const registerUser = asyncHandler(async (req ,res ) =>{
-//     res.status(200).json({
-//         messgae: "ok"
-//     })
-// })
-
-
-// export { registerUser }
 
 
 const registerUser = asyncHandler(async (req, res) => {
