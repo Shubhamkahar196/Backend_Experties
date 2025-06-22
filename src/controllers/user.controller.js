7e91d2ed07e9a68d35416f1cb47316d663d7148e
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-22 23:46:30 +0530

    Adding register User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,49 +2,55 @@
 // this is for error handling and avoid writing try catch every time
 import { asyncHandler } from '../utils/asyncHandler.js'
 
-// import {ApiError} from '../utils/apiError.js'
-// import {User } from "../models/user.model.js"
+ import {ApiError} from '../utils/apiError.js'
+ import {User } from "../models/user.model.js"
 
-// const registerUser = asyncHandler(async (req, res) => {
-    // get user details from frontend (Postman)
-    //validation - not empty 
-    //check if user already exists: username,email
-    //check for images,check for avatar
-    //upload them to cloudinary, avatar
-    //create user object - create entry in db
-    //remove password and refresh token field from response
-    //check for user creation
-    //return res
+ const registerUser = asyncHandler(async (req, res) => {
+    //  get user details from frontend (Postman)
+    // validation - not empty 
+    // check if user already exists: username,email
+    // check for images,check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
 
-    // const {fullname,email,password } = req.body;
-    // console.log("email:",email);
-    // console.log("fullname:",fullname);
-    // console.log("password:",password);
+     const {fullname,email,password } = req.body;
+     console.log("email:",email);
+     console.log("fullname:",fullname);
+     console.log("password:",password);
      
-    // if(fullname ===""){
-    //     throw new ApiError(400,"fullname is required")
-    // }
+     if(fullname ===""){
+         throw new ApiError(400,"fullname is required")
+     }
 
-    // if(
-    //     [fullname,email,username,password].some((field)=>
-    //         field?.trim()===""
-    //     )
-    // ){
-    //     throw new ApiError(400, "All fields are required")
-    // }
+     if(
+         [fullname,email,username,password].some((field)=>
+             field?.trim()===""
+         )
+     ){
+         throw new ApiError(400, "All fields are required")
+     }
 
+     const existUser = User.findOne({
+      $or: [{username}, {email}]
+     })
 
-
-// });
-
-// const registerUser = asyncHandler(async (req ,res ) =>{
-//     res.status(200).json({
-//         messgae: "ok"
-//     })
-// })
+     if(existUser){
+      throw new ApiError(409,"User already exists")
+     }
 
 
-// export { registerUser }
+
+     
+     
+ });
+
+
+
+
+ export { registerUser }
 
 
 // const registerUser = asyncHandler(async (req, res) => {
