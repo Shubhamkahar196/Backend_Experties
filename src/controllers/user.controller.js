646fbe8f2e314a905abaca79da29f95bb91e837a
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-23 11:28:56 +0530

    Adding register usercontroller and fixing bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,8 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 
  import {ApiError} from '../utils/apiError.js'
  import {User } from "../models/user.model.js"
+ import { uploadOnCloudinary } from '../utils/cloudinary.js';
+ import { ApiResponse } from '../utils/apiResponse.js';
 
  const registerUser = asyncHandler(async (req, res) => {
     //  get user details from frontend (Postman)
@@ -41,10 +43,50 @@ import { asyncHandler } from '../utils/asyncHandler.js'
       throw new ApiError(409,"User already exists")
      }
 
+   
+     // avatar 
+     const avatarLocalPath = req.files?.avatar[0]?.path;
 
+     //cover imgae
+     const  coverImageLocalPath = req.files?.coverImage[0]?.path;
 
+     if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar files is required")
+     }
      
      
+     //upload on cloudinary
+      const avatar = await uploadOnCloudinary(avatarLocalPath);
+      const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+      if(!avatar){
+        throw new ApiError(400,"Avatar files is required")
+      }
+
+      const user = await  User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+      })
+
+      // remove password and refresh token field from response
+      const createdUser =  await User.findById(user._id).select(
+        "-password  -refreshToken"
+        // "-password  -refreshToken" this is syntax for select which is not need 
+      )
+
+      if(!createdUser){
+        throw new ApiError(400,"Something went wrong during registering the user");
+      }
+
+
+      return res.status(201).json(   
+     new ApiResponse(200,createdUser, " user registered successfully")
+    )
+
  });
 
 
